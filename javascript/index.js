273a11693ca7c5105e21b1575ec6c57bab1b0753
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2023-06-29 15:32:45 +0530

    Made some changes and optimsied for screen view

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,8 @@
-// Request microphone access
 navigator.mediaDevices.getUserMedia({ audio: true })
     .then(function (stream) {
-
         let audioTrack = stream.getAudioTracks()[0];
-
         let microphoneName = audioTrack.label;
 
-        // mic name might change depending on browser as every api deals in a diff manner
         let button = document.getElementById("microphone-button");
         button.innerHTML = '<i class="fa-solid fa-microphone"></i>' + microphoneName;
 
@@ -15,32 +11,25 @@ navigator.mediaDevices.getUserMedia({ audio: true })
         });
     })
     .catch(function (error) {
-        // show alert of mic access is denied
         alert("Microphone access denied");
-        console.log(error)
+        console.log(error);
     });
-
-
-//   open and close settings and share  menu
 
 function handleMenuToggle(menuBtn, menu) {
-    menuBtn.addEventListener("click", function() {
-      menu.classList.toggle("active");
+    menuBtn.addEventListener("click", function () {
+        menu.classList.toggle("active");
     });
-  
-    document.addEventListener("click", function(event) {
-      const target = event.target;
-      if (!menu.contains(target) && !menuBtn.contains(target)) {
-        menu.classList.remove("active");
-      }
-    });
-  }
-  
-  const settingBtn = document.getElementById("settingsIcon");
-  const settingMenu = document.getElementById("settingMenu");
-  handleMenuToggle(settingBtn, settingMenu);
-  
-  const shareBtn = document.getElementById("shareIcon");
-  const shareMenu = document.getElementById("shareMenu");
-  handleMenuToggle(shareBtn, shareMenu);
-  
+}
+
+// Instead of using handleMenuToggle, use the onclick attribute directly
+const settingBtn = document.getElementById("settingsIcon");
+const settingMenu = document.getElementById("settingMenu");
+settingBtn.onclick = function () {
+    settingMenu.classList.toggle("active");
+};
+
+const shareBtn = document.getElementById("shareIcon");
+const shareMenu = document.getElementById("shareMenu");
+shareBtn.onclick = function () {
+    shareMenu.classList.toggle("active");
+};
