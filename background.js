260ef9628e65991df138814cb3529b4d7fa5284c
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-03-18 22:21:35 +0530

    fix: naming convention

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
         chrome.scripting.executeScript({
             target: { tabId },
-            files: ["./foreground.js"]
+            files: ["./contentscript.js"]
         }).then(() => {
             console.log("we have injected the content script");
         }).catch(err => console.log(err, "error in background script "))
